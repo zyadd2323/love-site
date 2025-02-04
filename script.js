2495b746dc2f002dfe55c5de065dc9df52887392
zyadd2323 <zyadth90@gmail.com>
2025-02-04 17:43:09 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("noButton").addEventListener("mouseover", function() {
+    let x = Math.random() * window.innerWidth;
+    let y = Math.random() * window.innerHeight;
+    this.style.position = "absolute";
+    this.style.left = `${x}px`;
+    this.style.top = `${y}px`;
+});
+
+document.getElementById("yesButton").addEventListener("click", function() {
+    alert("Yay! I knew you'd say YES! ❤️🎉");
+});
